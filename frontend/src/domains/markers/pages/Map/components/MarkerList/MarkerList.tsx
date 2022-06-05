f5a815ea5ca19c99e222a6e12fad6d70015c8dd6
v.shevchuk <v.shevchuk@abtollc.com>
2022-06-05 21:49:27 +0300

    marker lsit: improved responsiveness on tablets

--- frontend/src/domains/markers/pages/Map/components/MarkerList/MarkerList.tsx
+++ frontend/src/domains/markers/pages/Map/components/MarkerList/MarkerList.tsx
@@ -14,7 +14,7 @@ export default function MarkerList() {
             p-4 
             bottom-0 lg:top-0 lg:bottom-auto
             left-0 lg:right-0 lg:left-auto
-            w-full lg:w-1/5
+            w-full lg:w-1/3 xl:w-1/4 2xl:w-1/5
             h-1/2 lg:h-screen 
         `, (!visible && `
             w-[84px] lg:w-[84px]
